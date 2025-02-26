5db16054903b8dd9f843f546511c5cbece1109c9
matu.dev <matu.a.gimenez2002@gmail.com>
2025-02-25 22:18:38 -0300

    clase 25

--- clase-18-19-20-21-22-23-24-25/src/routes/workspace.router.js
+++ clase-18-19-20-21-22-23-24-25/src/routes/workspace.router.js
@@ -1,9 +1,12 @@
 import { Router } from "express"
 import { authMiddleware } from "../middlewares/authMiddleware.js"
-import { createWorkspaceController } from "../controllers/workspace.controller.js"
+import { createWorkspaceController, invteUserToWorkspaceController } from "../controllers/workspace.controller.js"
 
 const workspace_router = Router()
 
 workspace_router.post('/', authMiddleware, createWorkspaceController)
+
+// api/workspaces/invite/3123112dase3211
+workspace_router.post('/:workspace_id/invite/:invited_id', authMiddleware, invteUserToWorkspaceController)
 
 export default workspace_router
